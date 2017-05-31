7ed45cc60d8ae47b3c12ce09644ab9d2399791c3
nmmkk <nmmkk00@gmail.com>
2017-05-31 11:00:26 -0700

    Add a fish script to set color variables (with fixed values)

---
+++ fish/misc/set_color_variables.fish
@@ -0,0 +1,36 @@
+#!/usr/bin/env fish
+
+# fish_color_normal              normal
+# fish_color_command             F8F8F2
+# fish_color_quote               F1FA8C
+# fish_color_redirection         8BE9FD
+# fish_color_end                 50FA7B
+# fish_color_error               FFB86C
+# fish_color_param               FF79C6
+# fish_color_comment             6272A4
+# fish_color_match               --background=brblue
+# fish_color_search_match        bryellow --background=brblack
+# fish_color_operator            bryellow
+# fish_color_escape              bryellow --bold
+# fish_color_cwd                 green
+# fish_color_autosuggestion      BD93F9
+# fish_color_user                brgreen
+# fish_color_host                normal
+
+set fish_color_normal              normal
+set fish_color_command             F8F8F2
+set fish_color_quote               F1FA8C
+set fish_color_redirection         8BE9FD
+set fish_color_end                 50FA7B
+set fish_color_error               FFB86C
+set fish_color_param               FF79C6
+set fish_color_comment             6272A4
+set fish_color_match               --background=brblue
+set fish_color_search_match        bryellow --background=brblack
+set fish_color_operator            bryellow
+set fish_color_escape              bryellow --bold
+set fish_color_cwd                 green
+set fish_color_autosuggestion      BD93F9
+set fish_color_user                brgreen
+set fish_color_host                normal
+
