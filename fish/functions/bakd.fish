738ccb7c22a8c799e2e457a17b3faabd37aff1f5
nmmkk <nmmkk00@gmail.com>
2018-01-01 21:43:08 -0800

    fish: Get bakd() working on macOS

--- fish/functions/bakd.fish
+++ fish/functions/bakd.fish
@@ -1,11 +1,18 @@
 function bakd
     set -l directory {$PWD}/.backup_files
     set -l timestamp
+    set -l ls_cmd
+
+    if test (uname) = "Darwin"
+        set ls_cmd "/usr/local/bin/gls"
+    else
+        set ls_cmd "/bin/ls"
+    end
 
     mkdir -p {$directory}
 
     for f in $argv
-        set timestamp ( /bin/ls --full-time {$f} | awk '{print $6"T"$7}' )
+        set timestamp ( eval $ls_cmd --full-time {$f} | awk '{print $6"T"$7}' )
         mv {$f} {$directory}/{$f}.{$timestamp}; and cp -a {$directory}/{$f}.{$timestamp} {$f};
     end for
 end
