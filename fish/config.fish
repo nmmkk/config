8cd48cdb6d6f8f90757a8aded2a72896591725aa
nmmkk <nmmkk00@gmail.com>
2018-04-24 14:37:11 -0700

    fish: better handling of linuxbrew that has set up by bash

--- fish/config.fish
+++ fish/config.fish
@@ -25,10 +25,37 @@ end
 
 switch $os
 case Linux
-    # Originals so that we can restore it later
-    set -gx FISH_ORIG_PATH $PATH
-    set -gx FISH_ORIG_MANPATH $MANPATH
-    set -gx FISH_ORIG_INFOPATH $INFOPATH
+    # Take a backup of environment variables so that we can restore it later.
+    # Be warn that bash might have already enabled linuxbrew. So rather take a
+    # backup of environment variables named "ORIG_*" than of the real ones
+    # (like $PATH, $MANPATH, ... etc).
+    if not set -q ORIG_PATH
+        # Use eval to expand the value. Otherwise whole contents of "(echo
+        # $PATH | sed 's/:/ /g')" is packed into a single element.
+        eval set -gx ORIG_PATH (echo $PATH | sed 's/:/ /g')
+    else
+        eval set -gx ORIG_PATH (echo $ORIG_PATH | sed 's/:/ /g')
+    end
+
+    if not set -q ORIG_MANPATH
+        set -gx ORIG_MANPATH $MANPATH
+    end
+
+    if not set -q ORIG_INFOPATH
+        set -gx ORIG_INFOPATH $INFOPATH
+    end
+
+    if not set -q ORIG_PKG_CONFIG_PATH
+        set -gx ORIG_PKG_CONFIG_PATH $PKG_CONFIG_PATH
+    end
+
+    if not set -q ORIG_PERL5LIB
+        set -gx ORIG_PERL5LIB $PERL5LIB
+    end
+
+    if not set -q ORIG_PERL_LOCAL_LIB_ROOT
+        set -gx ORIG_PERL_LOCAL_LIB_ROOT $PERL_LOCAL_LIB_ROOT
+    end
 
     # Make empty directories silently to prevent errors in enable_linuxbrew()
     # on a Linux machine that does not have linuxbrew installed.
@@ -39,20 +66,44 @@ case Linux
         $HOME/.linuxbrew/share/info
 
     # Ones with linuxbrew enabled
-    set -gx FISH_LB_PATH  $HOME/.linuxbrew/sbin $HOME/.linuxbrew/bin $PATH
-    set -gx FISH_LB_MANPATH  $HOME/.linuxbrew/share/man $MANPATH
-    set -gx FISH_LB_INFOPATH  $HOME/.linuxbrew/share/info $INFOPATH
+    if not set -q LB_TOP
+        if test -d $HOME/.linuxbrew
+            set -gx LB_TOP $HOME/.linuxbrew
+        else if test -d /home/linuxbrew/.linuxbrew
+            set -gx LB_TOP /home/linuxbrew/.linuxbrew
+        end
+    end
+
+    # Use eval to expand the value. Otherwise whole contents of $ORIG_PATH goes
+    # into an element.
+    eval set -gx LB_PATH $LB_TOP/sbin $LB_TOP/bin (echo $ORIG_PATH | sed 's/:/ /g')
+    set -gx LB_MANPATH $LB_TOP/share/man $MANPATH
+    set -gx LB_INFOPATH $LB_TOP/share/info $INFOPATH
+    set -gx LB_PKG_CONFIG_PATH  $LB_TOP/opt/isl@0.18/lib/pkgconfig /usr/share/pkgconfig $ORIG_PKG_CONFIG_PATH
+
+    set -gx LB_PERL5_TOP $XDG_DATA_HOME/perl5-LB
+    set -gx LB_PERL5LIB $LB_PERL5_TOP/lib/perl5
+    set -gx LB_PERL_LOCAL_LIB_ROOT $LB_PERL5_TOP
+
+    set -gx HOMEBREW_NO_ANALYTICS 1
+    set -gx HOMEBREW_NO_EMOJI 1
 end
 
 #
 # Environment variables
 #
 
-set -gx XDG_DATA_HOME $HOME/.local/share
-set -gx XDG_CONFIG_HOME $HOME/.config
+if set -q XDG_DATA_HOME
+    set -gx XDG_DATA_HOME $HOME/.local/share
+end
+if set -q XDG_CONFIG_HOME
+    set -gx XDG_CONFIG_HOME $HOME/.config
+end
 # set -gx XDG_DATA_DIRS /usr/local/share/:/usr/share/
 # set -gx XDG_CONFIG_DIRS /etc/xdg
-set -gx XDG_CACHE_HOME $HOME/.cache
+if set -q XDG_CACHE_HOME
+    set -gx XDG_CACHE_HOME $HOME/.cache
+end
 
 #
 # Workarond for xsel error upon <C-w>, <C-u> ... etc.
@@ -76,9 +127,11 @@ function enable_linuxbrew
         return 0
     end
 
-    set PATH $FISH_LB_PATH
-    set MANPATH $FISH_LB_MANPATH
-    set INFOPATH $FISH_LB_INFOPATH
+    set -gx PATH $LB_PATH
+    set -gx MANPATH $LB_MANPATH
+    set -gx INFOPATH $LB_INFOPATH
+
+    set -gx LB_ENABLED 1
 end
 
 function disable_linuxbrew
@@ -87,9 +140,11 @@ function disable_linuxbrew
         return 0
     end
 
-    set PATH $FISH_ORIG_PATH
-    set MANPATH $FISH_ORIG_MANPATH
-    set INFOPATH $FISH_ORIG_INFOPATH
+    set -gx PATH $ORIG_PATH
+    set -gx MANPATH $ORIG_MANPATH
+    set -gx INFOPATH $ORIG_INFOPATH
+
+    set -gx LB_ENABLED
 end
 
 # [bashで言うところの!$はfishではどうすればよいか - Qiita](http://qiita.com/ymko/items/d7c5c4d0cc6174d5fc86)
