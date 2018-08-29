a6bc088046d9a604b461d8d86632b0800dc70228
nmmkk <nmmkk00@gmail.com>
2018-08-29 12:03:22 -0700

    zsh: add peco_select_history widget

--- zsh/oh-my-zsh/custom/custom-keybinding.zsh
+++ zsh/oh-my-zsh/custom/custom-keybinding.zsh
@@ -9,3 +9,11 @@ bindkey -e      ## Emacsライクキーバインド設定
 #### bindkey -a 'q' push-line    ## コマンドラインスタック
 #### bindkey "^R" history-incremental-search-backward    # Ctrl+R is incremental search like a bash.
 
+function peco_select_history() {
+    BUFFER=$(\history -r -n 1 | \
+        peco --layout=bottom-up --query "${LBUFFER}")
+    CURSOR=${#BUFFER}
+    zle clear-screen
+}
+zle -N peco_select_history
+bindkey '^r' peco_select_history
